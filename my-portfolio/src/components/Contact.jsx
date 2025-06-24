3238986c0e017c04687694c4dd875fd28c35c10d
ArnoVanSteenbergen03 <arno.vansteenbergen@student.arteveldehs.be>
2025-06-24 12:00:14 +0200

    changed some linking

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -6,11 +6,11 @@ function Contact() {
         <form className="contact__form" method="POST" name="contact" data-netlify="true">
           <div className="form__group">
             <label className="form__label">Name</label>
-            <input className="form__input" name="form-name" value="contact" required/>
+            <input className="form__input" name="form-name" required/>
           </div>
           <div className="form__group">
             <label className="form__label">Email</label>
-            <input className="form__input" name="form-email" value="contact" required />
+            <input className="form__input" name="form-email" required />
           </div>
           <div className="form__group">
             <label className="form__label">Message</label>
