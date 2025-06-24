35fd2b1d120cceea5c5d99894b141dbfbf2b2430
ArnoVanSteenbergen03 <arno.vansteenbergen@student.arteveldehs.be>
2025-06-24 11:47:11 +0200

    added netlify tag in my form

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -3,7 +3,7 @@ function Contact() {
     <section className="section" id="contact">
       <div className="contact">
         <h2 className="contact__title">Contact</h2>
-        <form className="contact__form" action="/process_form.php" method="POST">
+        <form className="contact__form" method="POST" netlify>
           <div className="form__group">
             <label className="form__label">Name</label>
             <input
